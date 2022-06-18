606a2718743f4be9d972b1178b8da95e019edbdf
KostyaLatushkin <107768186+KostyaLatushkin@users.noreply.github.com>
2022-06-18 20:45:39 +0300

    Create count.cpp

---
+++ 2laba/count.cpp
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <iostream>
+typedef unsigned long long ull;
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ull fst = 0ull,
+		scd = 1ull;
+	int count = 1;
+	cout << count++ << " : " << scd << '\n';
+	while (true) {
+		Sleep(100);
+		ull help = fst + scd;
+		fst = scd;
+		scd = help;
+		cout << count++ << " : " << help << '\n';
+	}
+}

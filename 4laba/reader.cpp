b250aa5f4fb91ef4752971421b7e44b8fbd5c3f0
KostyaLatushkin <107768186+KostyaLatushkin@users.noreply.github.com>
2022-06-18 20:47:40 +0300

    Create reader.cpp

---
+++ 4laba/reader.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+wchar_t const* mes[4]{
+	L"R_Exit",L"R_A",L"R_B",L"R_C"
+};
+
+int main() {
+	HANDLE hSemaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"Semaphore");
+	WaitForSingleObject(hSemaphore, INFINITE);
+	Sleep(50);
+	cout << "hi";
+	HANDLE mesEv[4];
+	for (int i = 0;i < 4;++i)
+		mesEv[i] = OpenEvent(EVENT_ALL_ACCESS, FALSE, mes[i]);
+	while (true) {
+		DWORD m = WaitForMultipleObjects(4, mesEv, FALSE, INFINITE);
+		if (m == WAIT_OBJECT_0)
+			break;
+		wcout << mes[m] + 2 << L'\n';
+	}
+	SetEvent(mesEv[0]);
+	ReleaseSemaphore(hSemaphore, 1, NULL);
+}

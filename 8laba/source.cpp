96c67b02762b795bc59c33d3f6d9e40b1feae884
KostyaLatushkin <107768186+KostyaLatushkin@users.noreply.github.com>
2022-06-18 20:51:03 +0300

    Create source.cpp

---
+++ 8laba/source.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+DWORD readCount = 0;
+HANDLE access = CreateSemaphore(NULL, 0, 1, NULL), 
+		readSum = CreateSemaphore(NULL, 0, 1, NULL);
+string text = "text";
+stringstream out;
+struct MyStruct
+{
+	int const p;
+};
+
+DWORD WINAPI writer(void*p) {
+	WaitForSingleObject(access, INFINITY);
+
+	text = string("text") + to_string(((MyStruct*)p)->p);
+	out << "Текст изменен на " << text << " \n";
+	Sleep(20);
+
+	ReleaseSemaphore(access, 1, NULL);
+	return 0;
+}
+DWORD WINAPI reader(void*) {
+	WaitForSingleObject(readSum, INFINITY);
+	++readCount;
+	if (readCount == 1)
+		WaitForSingleObject(access, INFINITY);
+	ReleaseSemaphore(readSum, 1, NULL);
+
+	out << "Прочитан текст " << text << "\n";
+	Sleep(20);
+
+	WaitForSingleObject(readSum, INFINITY);
+	--readCount;
+	if (readCount == 0)
+		ReleaseSemaphore(access, 1, NULL);
+	ReleaseSemaphore(readSum, 1, NULL);
+	return 0;
+}
+
+
+
+
+int main() {
+	
+	setlocale(LC_ALL, "ru");
+
+	cout << "Введите количество писателей и читателей:\n";
+	int nwriter, nreader;
+	cin >> nwriter >> nreader;
+	HANDLE* hwriter = new HANDLE[nwriter],
+		* hreader = new HANDLE[nreader];
+	DWORD* iwriter = new DWORD[nwriter],
+		* ireader = new DWORD[nreader];
+
+
+	for (int i = 0;i < nwriter / 2;++i)
+		hwriter[i] = CreateThread(NULL, NULL, writer, new MyStruct{ i }, 0, &iwriter[i]);
+	for (int i = 0;i < nreader;++i)
+		hreader[i] = CreateThread(NULL, NULL, reader, NULL, 0, &ireader[i]);
+	for (int i = nwriter / 2;i < nwriter;++i)
+		hwriter[i] = CreateThread(NULL, NULL, writer, new MyStruct{ i }, 0, &iwriter[i]);
+	
+	
+	WaitForMultipleObjects(nwriter, hwriter, TRUE, INFINITE);
+	WaitForMultipleObjects(nreader, hreader, TRUE, INFINITE);
+	cout << out.str();
+	for (int i = 0;i < nreader;++i)
+		CloseHandle(hreader[i]);
+	for (int i = 0;i < nwriter;++i)
+		CloseHandle(hwriter[i]);
+  CloseHandle(access);
+	CloseHandle(readSum);
+}

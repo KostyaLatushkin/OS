93d2cb6ee720e9c33339c19e286ebcfb05d50c0c
KostyaLatushkin <107768186+KostyaLatushkin@users.noreply.github.com>
2022-06-20 21:42:13 +0300

    Update new_standart.cpp

--- 1laba/new_standart.cpp
+++ 1laba/new_standart.cpp
@@ -3,14 +3,14 @@
 #include <chrono>
 #include <vector>
 
-void thread(std::vector<short> const& v, uint32_t const& len) {
+void thread(std::vector<short> const& v) {
 	int _count = 0;
-	for (int i = 0;i < len;++i) {
-		if (v[i] % 9 == 0 && v[i] < 0) {
+	for (auto val : v) {
+		if (val % 9 == 0 && val < 0) {
 			++_count;
-			std::cout << v[i] << '\n';
+			std::cout << val << '\n';
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(30));
+		std::this_thread::sleep_for(boost::chrono::microseconds(30));
 	}
 	if (!_count)
 		std::cout << "Элементы не найдены!\n";
@@ -26,7 +26,7 @@ int main() {
 		std::cin >> v[i];
 
 	std::cout << "Worker:\n";
-	std::thread thr(thread, std::cref(v), std::cref(n));
+	std::thread thr(thread, std::cref(v));
 
 	thr.join();
 }
